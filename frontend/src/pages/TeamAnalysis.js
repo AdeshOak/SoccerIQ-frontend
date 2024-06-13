09358f5db8488b5ddc6558b5865172073963df06
AdeshOak <adeshoak22@gmail.com>
2024-06-13 17:21:43 -0500

    Checking integration for frontend and backend after deploying, for first feature

--- frontend/src/pages/TeamAnalysis.js
+++ frontend/src/pages/TeamAnalysis.js
@@ -225,7 +225,7 @@ const TeamAnalysis = () =>{
             const data={
               'team': team};
             const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
-            console.log(backendUrl);
+            console.log('${backendUrl}/feature1');
             
             axios.post('${backendUrl}/feature1', data)
             .then(response => {
