5bebc12bb34574845ffda038f75f058b32c2710c
AdeshOak <adeshoak22@gmail.com>
2025-01-30 13:53:57 -0800

    Cleanup Navbar

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,50 +1,24 @@
 import React from 'react';
-//import { ReactComponent as logo } from "./logo.svg";
 import logo from '../SoccerIQ.png'; 
-
-
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink
-} from './NavbarElements';
-
+import { Nav, NavLink, Bars, NavMenu } from './NavbarElements';
+import './Navbar.css'; // Import CSS
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavLink to='/'>
-        <img src={logo} alt="SoccerIQ Logo" style={{ height: '70px', width: '70px' }} />
+          <img src={logo} alt="SoccerIQ Logo" className="nav-logo" />
         </NavLink>
         <Bars />
         <NavMenu>
-        <NavLink to='/' activeStyle>
-            Home
-          </NavLink>
-          <NavLink to='/feature1' activeStyle>
-            Team Analysis
-          </NavLink>
-          <NavLink to='/feature2' activeStyle>
-            Suited Player
-          </NavLink>
-          <NavLink to='/feature3' activeStyle>
-            Dream Team
-          </NavLink>
-          <NavLink to='/feature4' activeStyle>
-            Scout
-          </NavLink>
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+          <NavLink to='/' activeStyle>Home</NavLink>
+          <NavLink to='/feature1' activeStyle>Team Analysis</NavLink>
+          <NavLink to='/feature2' activeStyle>Suited Player</NavLink>
+          <NavLink to='/feature3' activeStyle>Dream Team</NavLink>
+          <NavLink to='/feature4' activeStyle>Scout</NavLink>
         </NavMenu>
-        {/*<NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-  </NavBtn>*/}
       </Nav>
-
     </>
   );
 };
